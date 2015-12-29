a98e9f2612ead7d5df19df85a5f3cdb44e96b1e5
tommysiu <20642193@A344C7LHV92PDB8.HBAP.ADROOT.HSBC>
2015-12-29 15:07:47 +0800

    Remove unused logger

--- microservice-day2/src/main/java/microservices/demo/CardService.java
+++ microservice-day2/src/main/java/microservices/demo/CardService.java
@@ -4,8 +4,6 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -14,8 +12,6 @@ import microservices.demo.model.Card;
 
 @Service
 public class CardService {
-
-	private static final Logger logger = LoggerFactory.getLogger(CardService.class);
 
 	public List<Card> getCardsOwnedBy(long userId) {
 		RestTemplate restTemplate = new RestTemplate();
